a9a0eaff59e7a645a73c567894242708f10689e6
Roberta Lima <rbnow12@gmail.com>
2022-05-03 20:55:42 -0300

    Implementa Tabuleiro e Peças

--- src/aplicacao/JogoXadrez.java
+++ src/aplicacao/JogoXadrez.java
@@ -1,11 +1,12 @@
 package aplicacao;
 
 import tabuleiro.Posicao;
+import tabuleiro.Tabuleiro;
 
 public class JogoXadrez {
     public static void main(String[] args){
 
-        Posicao posicao = new Posicao(3,5);
-        System.out.println(posicao);
+        Tabuleiro tabuleiro = new Tabuleiro(8,8);
+
     }
 }
