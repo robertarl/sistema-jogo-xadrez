1d0a282340b082ea487572ad340748082db30daf
Roberta Lima <rbnow12@gmail.com>
2022-05-03 20:28:01 -0300

    Primeira classe: Posição

---
+++ src/aplicacao/JogoXadrez.java
@@ -0,0 +1,11 @@
+package aplicacao;
+
+import tabuleiro.Posicao;
+
+public class JogoXadrez {
+    public static void main(String[] args){
+
+        Posicao posicao = new Posicao(3,5);
+        System.out.println(posicao);
+    }
+}
