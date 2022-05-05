5a7c504b6105ebd1a6d203dc3492ce1d4d82037b
Roberta Lima <rbnow12@gmail.com>
2022-05-05 11:00:39 -0300

    Limpando tela

--- src/aplicacao/JogoXadrez.java
+++ src/aplicacao/JogoXadrez.java
@@ -3,7 +3,9 @@ package aplicacao;
 import xadrez.PartidaXadrez;
 import xadrez.PecaXadrez;
 import xadrez.PosicaoXadrez;
+import xadrez.XadrezException;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class JogoXadrez {
@@ -13,17 +15,27 @@ public class JogoXadrez {
         PartidaXadrez partidaXadrez = new PartidaXadrez();
 
         while (true){
-            UI.printTabuleiro(partidaXadrez.getPecas());
-            System.out.println();
-            System.out.print("Origem: ");
-            PosicaoXadrez origem = UI.lerPosicaoXadrez(sc);
+            try {
+                UI.clearScreen();
+                UI.printTabuleiro(partidaXadrez.getPecas());
+                System.out.println();
+                System.out.print("Origem: ");
+                PosicaoXadrez origem = UI.lerPosicaoXadrez(sc);
 
-            System.out.println();
-            System.out.print("Destino: ");
-            PosicaoXadrez destino = UI.lerPosicaoXadrez(sc);
+                System.out.println();
+                System.out.print("Destino: ");
+                PosicaoXadrez destino = UI.lerPosicaoXadrez(sc);
 
-            PecaXadrez pecaCapturada = partidaXadrez.realizarPartida(origem,destino);
-
+                PecaXadrez pecaCapturada = partidaXadrez.realizarPartida(origem, destino);
+            }
+            catch (XadrezException e){
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            }
+            catch (InputMismatchException e){
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            }
         }
 
     }
--- src/aplicacao/UI.java
+++ src/aplicacao/UI.java
@@ -30,6 +30,12 @@ public class UI {
     public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
     public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
+    // https://stackoverflow.com/questions/2979383/java-clear-the-console
+    public static void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+
     public static PosicaoXadrez lerPosicaoXadrez(Scanner sc){
         try{
             String s = sc.nextLine();
@@ -37,7 +43,7 @@ public class UI {
             int linha = Integer.parseInt(s.substring(1));
             return new PosicaoXadrez(coluna, linha);
         } catch (RuntimeException e){
-            throw new InputMismatchException("Erro ao ler posição de xadrez. Valores válidos são de a1 até h8");
+            throw new InputMismatchException("Erro ao ler posicao de xadrez. Valores validos sao de a1 até h8");
         }
     }
 
